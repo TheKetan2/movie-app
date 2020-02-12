5e8e904780057fa111511dc41367cd3e553ff490
Ketan <ketantoo@gmail.com>
2020-02-12 12:59:55 +0530

    App finished

--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Header.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => (
   <div className="rmdb-header">
     <div className="rmdb-header-content">
       <Link to="/">
-        <img className="rmdb-logo" src="/images/reactMovie_logo.png" alt="rmdb-logo" />
+        <img
+          className="rmdb-logo"
+          src="https://github.com/TheKetan2/movie-app/blob/master/public/images/reactMovie_logo.png"
+          alt="rmdb-logo"
+        />
       </Link>
-      <img className="rmdb-tmdb-logo" src="/images/tmdb_logo.png" alt="tmdb-logo" />
+      <img
+        className="rmdb-tmdb-logo"
+        src="https://github.com/TheKetan2/movie-app/blob/master/public/images/tmdb_logo.png"
+        alt="tmdb-logo"
+      />
     </div>
   </div>
-)
+);
 
 export default Header;
