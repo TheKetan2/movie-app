1df593bb0c0e40f80db81cd197bf7844d8515afb
Ketan <ketantoo@gmail.com>
2020-02-12 14:36:14 +0530

    Updates

--- precache-manifest.7580f869046b9d33614a8996dcebf3fe.js
+++ precache-manifest.7580f869046b9d33614a8996dcebf3fe.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "765a47ba3b576e6320e4d603fcadd87e",
+    "revision": "a16a8c688c636f1591d40bf5ae7aa6ad",
     "url": "/movie-app/index.html"
   },
   {
-    "revision": "a1d26c38330aa702f20d",
+    "revision": "2498f797e7b3ca2bf97e",
     "url": "/movie-app/static/css/main.ffd96050.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/movie-app/static/js/2.44d29de1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a1d26c38330aa702f20d",
-    "url": "/movie-app/static/js/main.f30b0214.chunk.js"
+    "revision": "2498f797e7b3ca2bf97e",
+    "url": "/movie-app/static/js/main.9f7ea9e9.chunk.js"
   },
   {
     "revision": "50528415241a884c0652",
