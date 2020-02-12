b9260f8a96fec449b21e8667efde1aafd323b465
Ketan <ketantoo@gmail.com>
2020-02-12 12:24:37 +0530

    Updates

---
+++ precache-manifest.ad8ebb6d0f33b4e4dc98d32ca92e0342.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee7c103c540f8b284ae4adb01a5f3d93",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "a3f8499d70618b12ee8d",
+    "url": "/movie-app/static/css/main.ffd96050.chunk.css"
+  },
+  {
+    "revision": "984b9a4f7fac447b8107",
+    "url": "/movie-app/static/js/2.44d29de1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/movie-app/static/js/2.44d29de1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3f8499d70618b12ee8d",
+    "url": "/movie-app/static/js/main.a3d3a9fd.chunk.js"
+  },
+  {
+    "revision": "50528415241a884c0652",
+    "url": "/movie-app/static/js/runtime-main.68969649.js"
+  }
+]);
